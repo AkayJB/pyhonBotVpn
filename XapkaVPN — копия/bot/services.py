b9eab3e2536b0ffb42e91fe2ba7561d073a498fb
AkayJB <krupenkin2004@gmail.com>
2025-12-08 00:09:48 +0300

    Update services.py

--- XapkaVPN — копия/bot/services.py
+++ XapkaVPN — копия/bot/services.py
@@ -1,10 +1,19 @@
+"""
+СЕРВИСНЫЙ ФАЙЛ С ВСПОМОГАТЕЛЬНЫМИ ФУНКЦИЯМИ
+здесь может быть написана логика и работа с базой данных
+Сейчас содержит одну функцию для демонстрации архитектуры
+"""
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+# ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ ДЛЯ ПРОВЕРКИ СТАТУСА
 def get_subscription_status(user_id: int, subscriptions: dict) -> str:
     info = subscriptions.get(user_id)
+    # ПОДПИСКА АКТИВНА: возвращаем детали
     if info:
         username = info["username"]
         end_date = info["end_date"].strftime("%d.%m.%Y")
         return f"Пользователь: @{username}\nСтатус: Активирована\nДата окончания: {end_date}"
+
+     # ПОДПИСКИ НЕТ: стандартное сообщение
     return "Статус: Не активирована"
